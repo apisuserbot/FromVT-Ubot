6c6e182fd009b6d809ae8f747971360994917dc1
vckyouuu <viicky.auliaz@gmail.com>
2021-03-24 07:10:01 +0000

    pylint: auto fixes
    Signed-off-by: vckyouuu <viicky.auliaz@gmail.com>

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -119,10 +119,9 @@ async def repeat(rep):
 @register(outgoing=True, pattern="^.repo$")
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
-    await wannasee.edit(
-        "💎 **Repo Userbot :** [𝙁𝙧𝙤𝙢𝙑𝙏-𝙐𝙨𝙚𝙧𝘽𝙤𝙩](https://github.com/Vckyou/FromVT-Ubot)\n
-        "👑 **Pemilik      :** [𝙑𝙘𝙠𝙮𝙮𝙮](t.me/Vckyouuu)"
-    )
+    await wannasee.edit("💎 **Repo Userbot: ** [𝙁𝙧𝙤𝙢𝙑𝙏 - 𝙐𝙨𝙚𝙧𝘽𝙤𝙩](https: // github.com / Vckyou / FromVT - Ubot)\n
+                        "👑 **Pemilik      :** [𝙑𝙘𝙠𝙮𝙮𝙮](t.me/Vckyouuu)"
+                        )
 
 
 @register(outgoing=True, pattern="^.raw$")
