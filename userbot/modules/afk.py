4ca740216e38a887c869065c5ec98ceb335b94e0
vckyouuu <viicky.auliaz@gmail.com>
2021-03-24 03:15:43 +0000

    pylint: auto fixes
    Signed-off-by: vckyouuu <viicky.auliaz@gmail.com>

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -165,7 +165,7 @@ async def mention_afk(mention):
                 afk_since = f"`{int(seconds)} Detik`"
             if mention.sender_id not in USERS:
                 if AFKREASON:
-                    await mention.reply(f"🔥 𝘼 𝙁 𝙆"\n╭╼═══╾╼═══╾╼═══╾╼═══╾\n➥ {ALIVE_NAME} ꜱᴇᴅᴀɴɢ ᴀꜰᴋ\n➥ ꜱᴇᴊᴀᴋ : {afk_since}\n➥ ᴀʟᴀꜱᴀɴ : {AFKREASON}\n╰╼═══╾╼═══╾╼═══╾")
+                    await mention.reply(f"🔥 𝘼 𝙁 𝙆"\n╭╼═══╾╼═══╾╼═══╾╼═══╾\n➥ {ALIVE_NAME} ꜱᴇᴅᴀɴɢ ᴀꜰᴋ\n➥ ꜱᴇᴊᴀᴋ: {afk_since}\n➥ ᴀʟᴀꜱᴀɴ: {AFKREASON}\n╰╼═══╾╼═══╾╼═══╾")
                 else:
                     await mention.reply(str(choice(AFKSTR)))
                 USERS.update({mention.sender_id: 1})
@@ -173,7 +173,7 @@ async def mention_afk(mention):
             elif mention.sender_id in USERS:
                 if USERS[mention.sender_id] % randint(2, 4) == 0:
                     if AFKREASON:
-                        await mention.reply(f"🔥 𝘼 𝙁 𝙆"\n╭╼═══╾╼═══╾╼═══╾╼═══╾ \n➥ {ALIVE_NAME} ꜱᴇᴅᴀɴɢ ᴀꜰᴋ\n➥ ꜱᴇᴊᴀᴋ : {afk_since}\n➥ ᴀʟᴀꜱᴀɴ : {AFKREASON}\n╰╼═══╾╼═══╾╼═══╾")
+                        await mention.reply(f"🔥 𝘼 𝙁 𝙆"\n╭╼═══╾╼═══╾╼═══╾╼═══╾ \n➥ {ALIVE_NAME} ꜱᴇᴅᴀɴɢ ᴀꜰᴋ\n➥ ꜱᴇᴊᴀᴋ: {afk_since}\n➥ ᴀʟᴀꜱᴀɴ: {AFKREASON}\n╰╼═══╾╼═══╾╼═══╾")
                     else:
                         await mention.reply(str(choice(AFKSTR)))
                     USERS[mention.sender_id] = USERS[mention.sender_id] + 1
@@ -240,7 +240,7 @@ async def afk_on_pm(sender):
                 afk_since = f"`{int(seconds)} Detik`"
             if sender.sender_id not in USERS:
                 if AFKREASON:
-                    await sender.reply(f"🔥 𝘼 𝙁 𝙆"\n╭╼═══╾╼═══╾╼═══╾╼═══╾ \n➥ {ALIVE_NAME} ꜱᴇᴅᴀɴɢ ᴀꜰᴋ\n➥ ꜱᴇᴊᴀᴋ : {afk_since}\n➥ ᴀʟᴀꜱᴀɴ : {AFKREASON}\n╰╼═══╾╼═══╾╼═══╾")
+                    await sender.reply(f"🔥 𝘼 𝙁 𝙆"\n╭╼═══╾╼═══╾╼═══╾╼═══╾ \n➥ {ALIVE_NAME} ꜱᴇᴅᴀɴɢ ᴀꜰᴋ\n➥ ꜱᴇᴊᴀᴋ: {afk_since}\n➥ ᴀʟᴀꜱᴀɴ: {AFKREASON}\n╰╼═══╾╼═══╾╼═══╾")
                 else:
                     await sender.reply(str(choice(AFKSTR)))
                 USERS.update({sender.sender_id: 1})
@@ -248,7 +248,7 @@ async def afk_on_pm(sender):
             elif apprv and sender.sender_id in USERS:
                 if USERS[sender.sender_id] % randint(2, 4) == 0:
                     if AFKREASON:
-                        await sender.reply(f"🔥 𝘼 𝙁 𝙆"\n╭╼═══╾╼═══╾╼═══╾╼═══╾ \n➥ {ALIVE_NAME} ꜱᴇᴅᴀɴɢ ᴀꜰᴋ\n➥ ꜱᴇᴊᴀᴋ : {afk_since}\n➥ ᴀʟᴀꜱᴀɴ : {AFKREASON}\n╰╼═══╾╼═══╾╼═══╾")
+                        await sender.reply(f"🔥 𝘼 𝙁 𝙆"\n╭╼═══╾╼═══╾╼═══╾╼═══╾ \n➥ {ALIVE_NAME} ꜱᴇᴅᴀɴɢ ᴀꜰᴋ\n➥ ꜱᴇᴊᴀᴋ: {afk_since}\n➥ ᴀʟᴀꜱᴀɴ: {AFKREASON}\n╰╼═══╾╼═══╾╼═══╾")
                     else:
                         await sender.reply(str(choice(AFKSTR)))
                     USERS[sender.sender_id] = USERS[sender.sender_id] + 1
