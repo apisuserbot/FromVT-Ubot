c7e04215f396fe22d18bd788e8c7d9fe0c309512
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-03-28 04:26:42 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -296,9 +296,9 @@ async def amireallyalive(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     output = (
-        f"╭╼═══╾╼═══╾╼╾╼═══╾╼═══╾\n"
-        f"┃   **⚠️ 𝙁𝙍𝙊𝙈 𝙑𝙏  -  𝙐𝘽𝙊𝙏 ⚠️**\n"
-        f"┣╼═══╾╼═══╾╼╾╼═══╾╼═══╾ \n"
+        f"╭╼═══╾╼═══╾╼╾╼═══╾╼═══╾╮\n"
+        f"┃         **⚠️ 𝙁𝙍𝙊𝙈 𝙑𝙏  -  𝙐𝘽𝙊𝙏 ⚠️**         ┃\n"
+        f"┣╼═══╾╼═══╾╼╾╼═══╾╼═══╾╯ \n"
         f"┣❍ `Name     :` {DEFAULTUSER} \n"
         f"┣❍ `Username :` @{user.username} \n"
         f"┣❍ `Telethon :` Ver {version.__version__} \n"
@@ -306,9 +306,9 @@ async def amireallyalive(alive):
         f"┣❍ `Branch   :` {UPSTREAM_REPO_BRANCH} \n"
         f"┣❍ `Bot Ver  :` {BOT_VER} \n"
         f"┣❍ `Modules  :` {len(modules)} Modules \n"
-        f"┣╼═══╾╼═══╾╼╾╼═══╾╼═══╾ \n"
-        f"┣[𝗥𝗘𝗣𝗢](https://github.com/vckyou/FromVT-Ubot) | [𝗖𝗛𝗔𝗡𝗡𝗘𝗟](https://t.me/Vckyouuu) | [𝗣𝗘𝗠𝗜𝗟𝗜𝗞](t.me/Vckyouuu)\n"
-        f"╰╼════╾╼═══╾╼═══╾╼════╾")
+        f"┣╼═══╾╼═══╾╼╾╼═══╾╼═══╾╮ \n"
+        f"┣[𝗥𝗘𝗣𝗢](https://github.com/vckyou/FromVT-Ubot) | [𝗖𝗛𝗔𝗡𝗡𝗘𝗟](https://t.me/Vckyouuu) | [𝗣𝗘𝗠𝗜𝗟𝗜𝗞](t.me/Vckyouuu)         ┃\n"
+        f"╰╼════╾╼═══╾╼═══╾╼════╾╯")
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
