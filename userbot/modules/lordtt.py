dd045130580351263a3b30b1bb15d6c46860435a
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-03-29 03:40:31 +0700

    Update lordtt.py

--- userbot/modules/lordtt.py
+++ userbot/modules/lordtt.py
@@ -3,7 +3,7 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
-# Lord Userbot
+# FromVT-UserBot
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot import bot, CMD_HELP
