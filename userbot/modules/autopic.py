7aa13a70993c5997c2233aab4e5f96374325dc99
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-04-02 00:14:23 +0700

    Update autopic.py

--- userbot/modules/autopic.py
+++ userbot/modules/autopic.py
@@ -15,10 +15,10 @@ from userbot import CMD_HELP, bot
 async def autopic(e):
     search = e.pattern_match.group(1)
     if not search:
-        return await eor(get_string("autopic_1"))
+        return await event.edit(get_string("autopic_1"))
     clls = returnpage(search)
     if len(clls) == 0:
-        return await eor(get_string("autopic_2").format(search))
+        return await event.edit(get_string("autopic_2").format(search))
     if not len(clls) == 1:
         num = random.randrange(0, len(clls) - 1)
     else:
@@ -42,9 +42,9 @@ async def autopic(e):
 async def stoppo(ult):
     gt = udB.get("AUTOPIC")
     if not gt == "True":
-        return await eor(ult, "`AUTOPIC was not in used !!`")
+        return await event.edit(ult, "`AUTOPIC was not in used !!`")
     udB.set("AUTOPIC", "None")
-    await eor(ult, "`AUTOPIC Stopped !!`")
+    await event.edit(ult, "`AUTOPIC Stopped !!`")
 
 
 CMD_HELP.update({"autopic": "`.autopic` : `.stoppic`\
