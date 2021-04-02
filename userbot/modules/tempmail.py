a0cfe1b673ca3e71bfd9f0f74e56327c801752ae
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-04-03 00:37:26 +0700

    Update tempmail.py

--- userbot/modules/tempmail.py
+++ userbot/modules/tempmail.py
@@ -1,17 +1,21 @@
-# pikiiii ganteng
+# BASED FROM ULTROID PORTED FOR FromVT-Userbot BY ALVIN / @LIUALVINAS
+# THANKS FOR TEAMULTROID
+# DONT REMOVE THIS
+# PIKI GANS
+# @LORDUSERBOT_GROUP
 
-
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot, CMD_HELP
+from telethon import events
+from userbot import CMD_HELP, bot
 from userbot.events import register
-
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+import asyncio
 # piki
 
 
 @register(outgoing=True, pattern="^.tempmail ?(.*)")
 async def demn(event):
     chat = "@TempMailBot"
-    await event.edit("Sedang Memprosess...")
+    fromvt = await event.edit("Sedang Memprosess...")
     async with bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(events.NewMessage(
@@ -22,11 +26,11 @@ async def demn(event):
             await conv.send_message("/start")
             await asyncio.sleep(1)
             await conv.send_message("Generate New")
-            response = await conv.get_response()
-            ((response).reply_markup.rows[2].buttons[0].url)
-            await bot.send_read_acknowledge(chat)
+            response = await response
+            fromvt = ((response).reply_markup.rows[2].buttons[0].url)
+            await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit("Mohon Unblock @TempMailBot !!!")
+            await fromvt.edit("Mohon Unblock @TempMailBot !!!")
             return
         await event.edit(f"TEMPMAIL ~ `{response.message.message}`\n\n[KLIK DISINI UNTUK MELIHAT VERIFIKASI]({link})")
 
