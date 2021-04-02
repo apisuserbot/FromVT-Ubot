650b11c94d07cfc3eaef7f6355eb8346bdbc238a
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-04-02 17:39:28 +0700

    Create tempmail.py

---
+++ userbot/modules/tempmail.py
@@ -0,0 +1,34 @@
+from userbot import bot, CMD_HELP
+from userbot.events import register
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+import asyncio
+
+
+@register(outgoing=True, pattern="^.tempmail ?(.*)")
+async def demn(event):
+    chat = "@TempMailBot"
+    msg = await event.edit(ult, "Sedang Diprosess...")
+    async with bot.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(events.NewMessage(
+                incoming=True,
+                from_users=220112646
+            )
+            )
+            await conv.send_message("/start")
+            await asyncio.sleep(1)
+            await conv.send_message("Generate New")
+            response = await response
+            link = ((response).reply_markup.rows[2].buttons[0].url)
+            await bot.send_read_acknowledge(chat)
+        except YouBlockedUserError:
+            await event.edit("Mohon Unblock @TempMailBot !!!")
+            return
+        await event.edit(ult, f"TEMPMAIL ~ `{response.message.message}`\n\n[KLIK DISINI UNTUK MELIHAT VERIFIKASI]({link})")
+
+
+# Alvin Gans
+CMD_HELP.update(
+    {
+        "tempmail": "**Modules:** __Temp Mail__\n\n**Perintah:** `.tempmail Generate TempMail`"
+        "\n**Penjelasan:** Mendapatkan Email Gratis Dari Temp Mail"})
