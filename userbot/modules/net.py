417891c2e40a5d15735e91f9c70579798d7a2052
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-04-05 05:39:26 +0700

    Create net.py

---
+++ userbot/modules/net.py
@@ -0,0 +1,40 @@
+Port By : Piki Ganteng
+
+import random
+
+from plugins.stickertools import deEmojify
+from telethon.errors import ChatSendInlineForbiddenError, ChatSendStickersForbiddenError
+
+from userbot.events import register
+from userbot import CMD_HELP
+
+
+@register(outgoing=True, pattern=r"^\.net (.*)")
+async def nope(fromvt):
+    vt = fromvt.pattern_match.group(1)
+    a = await event.edit("`Processing...`")
+    if not vt:
+        if fromvt.is_reply:
+            (await fromvt.get_reply_message()).message
+        else:
+            return await event.edit("`Sir please give some query to search and download it for you..!`",
+            )
+    sticcers = await bot.inline_query("Lybot", f"{(deEmojify(vt))}")
+    try:
+        await sticcers[0].click(
+            fromvt.chat_id,
+            reply_to=doit.reply_to_msg_id,
+            silent=True if doit.is_reply else False,
+            hide_via=True,
+        )
+        await event.delete()
+    except ChatSendInlineForbiddenError:
+        await event.edit("`Boss ! I cant use inline things here...`")
+
+
+CMD_HELP.update(
+    {
+        "net": ">`.net <Judul Lagu>`"
+        "\nUsage: Mendownload Music"
+    }
+)
