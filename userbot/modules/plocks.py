a141f945171d2638c819a3cf45c737a662e61e28
vckyouuu <viicky.auliaz@gmail.com>
2021-04-05 04:01:55 +0000

    pylint: auto fixes
    Signed-off-by: vckyouuu <viicky.auliaz@gmail.com>

--- userbot/modules/plocks.py
+++ userbot/modules/plocks.py
@@ -161,7 +161,7 @@ async def _(event):
                                     )
         if uchangeinfo:
             return await event.edit("`This user is already locked with Changing group info by users`",
-                                     )
+                                    )
         uchangeinfo = True
         locktype = "chat info"
     elif input_str == "all":
@@ -207,63 +207,63 @@ async def _(event):
         await event.edit_or_reply(f"`Locked {locktype} for this user !!`")
     except BaseException as e:
         await event.edit(f"`Do I have proper rights for that ??`\n\n**Error:** `{str(e)}`",
-                         time = 5,
+                         time=5,
                          )
 
 
-@register(outgoing = True, pattern = r"^\.unplock(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.unplock(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
-    input_str=event.pattern_match.group(1)
-    peer_id=event.chat_id
-    reply=await event.get_reply_message()
+    input_str = event.pattern_match.group(1)
+    peer_id = event.chat_id
+    reply = await event.get_reply_message()
     if not event.is_group:
         return await edit_delete(event, "`Idiot! ,This is not a group to lock things `")
-    chat_per=(await event.get_chat()).default_banned_rights
-    result=await event.client(
+    chat_per = (await event.get_chat()).default_banned_rights
+    result = await event.client(
         functions.channels.GetParticipantRequest(
             channel=peer_id, user_id=reply.from_id)
     )
-    admincheck=await is_admin(event.client, peer_id, reply.from_id)
+    admincheck = await is_admin(event.client, peer_id, reply.from_id)
     if admincheck:
         return await event.edit("`This user is admin you cant play with him`")
-    fromvt=base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-    msg=chat_per.send_messages
-    media=chat_per.send_media
-    sticker=chat_per.send_stickers
-    gif=chat_per.send_gifs
-    gamee=chat_per.send_games
-    ainline=chat_per.send_inline
-    embed_link=chat_per.embed_links
-    gpoll=chat_per.send_polls
-    adduser=chat_per.invite_users
-    cpin=chat_per.pin_messages
-    changeinfo=chat_per.change_info
+    fromvt = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+    msg = chat_per.send_messages
+    media = chat_per.send_media
+    sticker = chat_per.send_stickers
+    gif = chat_per.send_gifs
+    gamee = chat_per.send_games
+    ainline = chat_per.send_inline
+    embed_link = chat_per.embed_links
+    gpoll = chat_per.send_polls
+    adduser = chat_per.invite_users
+    cpin = chat_per.pin_messages
+    changeinfo = chat_per.change_info
     try:
-        umsg=result.participant.banned_rights.send_messages
-        umedia=result.participant.banned_rights.send_media
-        usticker=result.participant.banned_rights.send_stickers
-        ugif=result.participant.banned_rights.send_gifs
-        ugamee=result.participant.banned_rights.send_games
-        uainline=result.participant.banned_rights.send_inline
-        uembed_link=result.participant.banned_rights.embed_links
-        ugpoll=result.participant.banned_rights.send_polls
-        uadduser=result.participant.banned_rights.invite_users
-        ucpin=result.participant.banned_rights.pin_messages
-        uchangeinfo=result.participant.banned_rights.change_info
+        umsg = result.participant.banned_rights.send_messages
+        umedia = result.participant.banned_rights.send_media
+        usticker = result.participant.banned_rights.send_stickers
+        ugif = result.participant.banned_rights.send_gifs
+        ugamee = result.participant.banned_rights.send_games
+        uainline = result.participant.banned_rights.send_inline
+        uembed_link = result.participant.banned_rights.embed_links
+        ugpoll = result.participant.banned_rights.send_polls
+        uadduser = result.participant.banned_rights.invite_users
+        ucpin = result.participant.banned_rights.pin_messages
+        uchangeinfo = result.participant.banned_rights.change_info
     except AttributeError:
-        umsg=msg
-        umedia=media
-        usticker=sticker
-        ugif=gif
-        ugamee=gamee
-        uainline=ainline
-        uembed_link=embed_link
-        ugpoll=gpoll
-        uadduser=adduser
-        ucpin=cpin
-        uchangeinfo=changeinfo
+        umsg = msg
+        umedia = media
+        usticker = sticker
+        ugif = gif
+        ugamee = gamee
+        uainline = ainline
+        uembed_link = embed_link
+        ugpoll = gpoll
+        uadduser = adduser
+        ucpin = cpin
+        uchangeinfo = changeinfo
     if input_str == "msg":
         if msg:
             return await event.edit("`This Group is locked with messaging permission.`"
@@ -271,16 +271,16 @@ async def _(event):
         if not umsg:
             return await event.edit("`This User is already unlocked with messaging permission.`"
                                     )
-        umsg=False
-        locktype="messages"
+        umsg = False
+        locktype = "messages"
     elif input_str == "media":
         if media:
             return await event.edit("`This Group is locked with sending media`")
         if not umedia:
             return await event.edit("`User is already unlocked with sending media`"
                                     )
-        umedia=False
-        locktype="media"
+        umedia = False
+        locktype = "media"
     elif input_str == "sticker":
         if sticker:
             return await event.edit("`This Group is locked with sending stickers`"
@@ -288,8 +288,8 @@ async def _(event):
         if not usticker:
             return await event.edit("`This user is already unlocked with sending stickers`"
                                     )
-        usticker=False
-        locktype="stickers"
+        usticker = False
+        locktype = "stickers"
     elif input_str == "preview":
         if embed_link:
             return await event.edit("`This Group is locked with previewing links`"
@@ -297,24 +297,24 @@ async def _(event):
         if not uembed_link:
             return await event.edit("`This user is already unlocked with previewing links`"
                                     )
-        uembed_link=False
-        locktype="preview links"
+        uembed_link = False
+        locktype = "preview links"
     elif input_str == "gif":
         if gif:
             return await event.edit("`This Group is locked with sending GIFs`")
         if not ugif:
             return await event.edit("`This user is already unlocked with sending GIFs`"
                                     )
-        ugif=False
-        locktype="GIFs"
+        ugif = False
+        locktype = "GIFs"
     elif input_str == "game":
         if gamee:
             return await event.edit("`This Group is locked with sending games`")
         if not ugamee:
             return await event.edit("`This user is already unlocked with sending games`"
                                     )
-        ugamee=False
-        locktype="games"
+        ugamee = False
+        locktype = "games"
     elif input_str == "inline":
         if ainline:
             return await event.edit("`This Group is locked with using inline bots`"
@@ -322,16 +322,16 @@ async def _(event):
         if not uainline:
             return await event.edit("`This user is already unlocked with using inline bots`"
                                     )
-        uainline=False
-        locktype="inline bots"
+        uainline = False
+        locktype = "inline bots"
     elif input_str == "poll":
         if gpoll:
             return await event.edit("`This Group is locked with sending polls`")
         if not ugpoll:
             return await event.edit("`This user is already unlocked with sending polls`"
                                     )
-        ugpoll=False
-        locktype="polls"
+        ugpoll = False
+        locktype = "polls"
     elif input_str == "invite":
         if adduser:
             return await event.edit("`This Group is locked with adding members`"
@@ -339,8 +339,8 @@ async def _(event):
         if not uadduser:
             return await event.edit("`This user is already unlocked with adding members`"
                                     )
-        uadduser=False
-        locktype="invites"
+        uadduser = False
+        locktype = "invites"
     elif input_str == "pin":
         if cpin:
             return await event.edit("`This Group is locked with pinning messages by users`",
@@ -348,8 +348,8 @@ async def _(event):
         if not ucpin:
             return await event.edit("`This user is already unlocked with pinning messages by users`",
                                     )
-        ucpin=False
-        locktype="pins"
+        ucpin = False
+        locktype = "pins"
     elif input_str == "info":
         if changeinfo:
             return await event.edit("`This Group is locked with Changing group info by users`",
@@ -357,44 +357,44 @@ async def _(event):
         if not uchangeinfo:
             return await event.edit("`This user is already unlocked with Changing group info by users`",
                                     )
-        uchangeinfo=False
-        locktype="chat info"
+        uchangeinfo = False
+        locktype = "chat info"
     elif input_str == "all":
         if not msg:
-            umsg=False
+            umsg = False
         if not media:
-            umedia=False
+            umedia = False
         if not sticker:
-            usticker=False
+            usticker = False
         if not gif:
-            ugif=False
+            ugif = False
         if not gamee:
-            ugamee=False
+            ugamee = False
         if not ainline:
-            uainline=False
+            uainline = False
         if not embed_link:
-            uembed_link=False
+            uembed_link = False
         if not gpoll:
-            ugpoll=False
+            ugpoll = False
         if not adduser:
-            uadduser=False
+            uadduser = False
         if not cpin:
-            ucpin=False
+            ucpin = False
         if not changeinfo:
-            uchangeinfo=False
-        locktype="everything"
+            uchangeinfo = False
+        locktype = "everything"
     else:
         if input_str:
-            return await event.edit(f"**Invalid lock type :** `{input_str}`", time = 5
+            return await event.edit(f"**Invalid lock type :** `{input_str}`", time=5
                                     )
 
         return await event.edit_or_reply("`I can't lock nothing !!`")
     try:
-        fromvt=Get(fromvt)
+        fromvt = Get(fromvt)
         await event.client(fromvt)
     except BaseException:
         pass
-    lock_rights=ChatBannedRights(
+    lock_rights = ChatBannedRights(
         until_date=None,
         send_messages=umsg,
         send_media=umedia,
