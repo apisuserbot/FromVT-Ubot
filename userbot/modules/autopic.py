f9836497aa558b6fabc49c8246e74afab8893118
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-04-07 06:13:51 +0700

    Create autopic.py

---
+++ userbot/modules/autopic.py
@@ -0,0 +1,47 @@
+# wahahaha
+#port by @Vckyouuu
+
+import asyncio
+import os
+import random
+
+from telethon import functions
+
+from userbot.events import register
+from userbot import CMD_HELP
+
+
+@register(outgoing=True, pattern="^.newbot ?(.*)")
+async def autopic(event):
+    search = event.pattern_match.group(1)
+    if not search:
+        return await event.edit(get_string("autopic_1"))
+    clls = returnpage(search)
+    if len(clls) == 0:
+        return await event.edit(get_string("autopic_2").format(search))
+    if not len(clls) == 1:
+        num = random.randrange(0, len(clls) - 1)
+    else:
+        num = 0
+    page = clls[num]
+    title = page["title"]
+    await event.edit(get_string("autopic_3").format(title))
+    udB.set("AUTOPIC", "True")
+    while True:
+        ge = udB.get("AUTOPIC")
+        if not ge == "True":
+            return
+        animepp(page["href"])
+        file = await bot.upload_file("autopic.jpg")
+        await bot(functions.photos.UploadProfilePhotoRequest(file))
+        os.remove("autopic.jpg")
+        await asyncio.sleep(1100)
+
+
+@ultroid_cmd(pattern="stoppic$")
+async def stoppo(event):
+    gt = udB.get("AUTOPIC")
+    if not gt == "True":
+        return await event.edit("`AUTOPIC was not in used !!`")
+    udB.set("AUTOPIC", "None")
+    await event.edit("`AUTOPIC Stopped !!`")
