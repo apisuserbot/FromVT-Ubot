1abe77c8c8b80dd456289ae153a67280496f97e3
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-04-06 15:22:50 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -123,7 +123,7 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@ @ register(outgoing=True, pattern="^.ping$")
+@register(outgoing=True, pattern="^.ping$")
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     uptime = await get_readable_time((time.time() - StartTime))
